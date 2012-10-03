d07a5cf313651887f2d7076d3384c7975863feff
Nick Klauer <klauer@gmail.com>
2012-10-02 23:13:48 -0500

    add documentation comments
    Explaining what I want to do, not how I want to do it.  More ranting, basically.

--- test/com/klauer/reveille/core_test.clj
+++ test/com/klauer/reveille/core_test.clj
@@ -1,5 +1,13 @@
 (ns com.klauer.reveille.core-test
   (:use [midje.sweet]))
 
+;; how to use midje to test out the expressions I want to simplify the DSL with
+;; maybe I could emulate or integrate https://github.com/aredington/monotony.
+;; does monotony do what I need to do already?  It seems more geared to tease out times,
+;; not much in the way of scheduling events based on them.  Either that, or the docs are
+;; incredibly poor at showing how to utilize a schedule item somewhere.
 
 
+;; just reading the docs, it seems that it's a very interesting wrapper around
+;; expressive representations of timed events.  So it's more about defining WHEN,
+;; not wrapping a WHAT around it.  Maybe it will work, maybe not.
