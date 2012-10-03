e0e34799cc198b4fe1d0888e62215f40aa0e014b
Nick Klauer <klauer@gmail.com>
2012-10-02 21:52:41 -0500

    initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to reveille
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/reveille/core_test.clj
@@ -0,0 +1,7 @@
+(ns reveille.core-test
+  (:use clojure.test
+        reveille.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
